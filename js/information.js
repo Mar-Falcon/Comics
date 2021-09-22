3d7dbccd28cd99411542220a1a152cb16826bf6c
Maia <maiafleschler@gmail.com>
2021-09-22 18:15:31 -0300

    Add Not Results Found at info

--- js/information.js
+++ js/information.js
@@ -99,7 +99,6 @@ var createComicInfo = function (element) {
         return "   " + items.name;
     };
     var writerData = element.creators.items.map(formatName);
-    console.log(element.creators.items[0]);
     var writerTxt = document.createTextNode(writerData);
     writer.appendChild(writerTxt);
     info.appendChild(writer);
@@ -111,6 +110,17 @@ var createComicInfo = function (element) {
     description.appendChild(descriptionTxt);
     info.appendChild(description);
     cardInfo.appendChild(info);
+};
+var cardsRelated = function (response) {
+    if (response.data.total === 0) {
+        var notResults = document.createElement('h2');
+        var txtNotResults = document.createTextNode('No results found');
+        notResults.appendChild(txtNotResults);
+        cardsContainer.appendChild(notResults);
+    }
+    else {
+        createCards(offset, response);
+    }
 };
 var getCardData = function (e) { return __awaiter(_this, void 0, void 0, function () {
     var card, characterId, queryParams, methodComicId, methodComicIdCharacters, comicResponse, dataComic, charactersResponse, methodCharacterId, methodCharacterIdComics, characterResponse, dataCharacter, comicsResponse;
@@ -126,7 +136,6 @@ var getCardData = function (e) { return __awaiter(_this, void 0, void 0, functio
                 return [4 /*yield*/, getData(offset, methodComicId, queryParams)];
             case 1:
                 comicResponse = _a.sent();
-                console.log(comicResponse);
                 dataComic = comicResponse.data.results;
                 return [4 /*yield*/, getData(offset, methodComicIdCharacters, queryParams)];
             case 2:
@@ -134,7 +143,7 @@ var getCardData = function (e) { return __awaiter(_this, void 0, void 0, functio
                 cardsContainer.innerHTML = "";
                 cardsSectionSubTitle.innerHTML = "Characters";
                 createComicInfo(dataComic[0]);
-                createCards(offset, charactersResponse);
+                cardsRelated(charactersResponse);
                 return [3 /*break*/, 6];
             case 3:
                 methodCharacterId = "/v1/public/characters/" + characterId + "?";
@@ -149,7 +158,7 @@ var getCardData = function (e) { return __awaiter(_this, void 0, void 0, functio
                 cardsContainer.innerHTML = "";
                 cardsSectionSubTitle.innerHTML = "Comics";
                 createCharacterInfo(dataCharacter[0]);
-                createCards(offset, comicsResponse);
+                cardsRelated(comicsResponse);
                 _a.label = 6;
             case 6: return [2 /*return*/];
         }
